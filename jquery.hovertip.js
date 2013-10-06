12d043876e68cfe0eb49114d8b966689eae922ec
harikr2006 <harikr2006@gmail.com>
2013-10-06 23:07:58 +0530

    Tooltip

---
+++ jquery.hovertip.js
@@ -0,0 +1,58 @@
+// JavaScript Document
+(function ($) {
+    $.fn.hovertip = function (options) {
+        var defaults = $.extend({ 
+			setAttr:"title",           
+            classId: 'xhover',
+            speed: 300,
+            padxy: 5,
+            bgcolor: "rgba(0,0,0,0.8)",
+            borderRadius: 3,
+            textColor: "#fff",
+			maxWidth:100,
+			fontSize:12,
+			fontFamily:"Halvetica",
+			Border:"1px solid #ccc",
+			boxShadow:"0px 0px 3px #000"
+        },options);		
+		return this.each(
+			function(){
+		var $wrapper = "<div id='" + defaults.classId + "'></div>";
+        var xtitle;
+		var o=options;
+        $(this).on('mouseover',
+            function (e) {
+                xtitle = $(this).attr(defaults.setAttr);
+                //console.log($('#mypop').length);
+                if ($('#' + defaults.classId).length <= 0) {
+                    $(this).parent().append($wrapper).find('#' + defaults.classId).hide();
+                }
+                $('#' + defaults.classId).append(xtitle);
+                $('#' + defaults.classId)
+                    .css({						
+                        "position": "absolute",
+                        "top": +e.clientY + (10) + "px",
+                        "left": +e.clientX - ($('#' + defaults.classId).width()/2) + "px",
+                        "padding": defaults.padxy,
+                        "background": defaults.bgcolor,
+                        "border-radius": defaults.borderRadius,
+                        "color": defaults.textColor,						
+						"max-width":defaults.maxWidth+"px",
+						"font-size":defaults.fontSize+"px",
+						"font-family":defaults.fontFamily,
+						"box-shadow":defaults.boxShadow,
+						"border":defaults.Border
+                    }).fadeTo(defaults.speed, 1);					
+				$(this).css({"cursor":"pointer"});
+                $(this).removeAttr(defaults.setAttr);
+            }
+        );
+        $(this).on('mouseout',
+            function () {
+                $('#' + defaults.classId).fadeTo(defaults.speed, 0);
+                $('#' + defaults.classId).remove();
+                $(this).attr(defaults.setAttr, xtitle);
+            })}		
+		);
+    }
+}(jQuery));
